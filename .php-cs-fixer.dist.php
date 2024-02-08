126cfdab499f696799a183a3a9e462d6e60d7373
MarkejN <markus.nestmann@outlook.com>
2024-02-08 13:04:04 +0100

    Require contao 5

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -3,17 +3,17 @@
 declare(strict_types=1);
 
 $header = <<<'EOF'
-    This file is part of postyou/contao-proven-expert
+    This file is part of postyou/contao-proven-expert.
 
-    (c) POSTYOU Digital- & Filmagentur
+    (c) POSTYOU Werbeagentur
 
     @license LGPL-3.0+
     EOF;
 
 $finder = PhpCsFixer\Finder::create()
-    ->exclude('templates')
     ->in([
         __DIR__.'/src',
+        __DIR__.'/contao',
     ])
 ;
 
@@ -22,11 +22,13 @@ $config = new PhpCsFixer\Config();
 return $config
     ->setRiskyAllowed(true)
     ->setRules([
-        '@PHP73Migration' => true,
-        // '@PHP82Migration' => true,
-        // '@PHP80Migration:risky' => true,
+        '@PHP82Migration' => true,
+        '@PHP80Migration:risky' => true,
         '@PhpCsFixer' => true,
         '@PhpCsFixer:risky' => true,
+        'echo_tag_syntax' => ['format' => 'short'],
+        'no_alternative_syntax' => ['fix_non_monolithic_code' => false],
+        'semicolon_after_instruction' => false,
         'header_comment' => ['header' => $header],
     ])
     ->setFinder($finder)
